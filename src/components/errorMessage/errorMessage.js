8590d4765a1b278bb85fc7d020e5f97409e2e67a
Sergei <amonr51@gmail.com>
2019-02-10 22:22:17 +0300

    переместить обработчик ошибки в модуль ошибки

--- src/components/errorMessage/errorMessage.js
+++ src/components/errorMessage/errorMessage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './errorMessage.css';
-// import img from './error'
+import img from './error.jpg'
 
 const ErrorMessage = () => {
     return (
         <>
-            <img src={process.env.PUBLIC_URL + '/img/error2.jpg'} alt="error"></img>
+            <img src={img} alt="error"></img>
             <span>Случилась ошибка</span>
         </>
     )
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -34,8 +34,8 @@ export default class RandomChar extends Component {
     }
 
     updateChar() {
-        // const id = Math.floor(Math.random() * 140 + 25);
-        const id = 10000000000000;
+        const id = Math.floor(Math.random() * 140 + 25);
+        // const id = 10000000000000;
         this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
