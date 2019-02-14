4c490f5431aa16719bb1eeba9cdf4788fe8dc981
Sergei <amonr51@gmail.com>
2019-02-14 22:39:45 +0300

    реакт .props.children example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Example extends Component {
+    render() {
+        return this.props.children;
+    }
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Example>
+    <h1>Hello</h1>
+    [1,2,3]
+    `{{tttt: "1", a: 'a'}}`
+</Example>, document.getElementById('root'));
