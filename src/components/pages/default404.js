a5ffab7e61fc7f5fa245455c7ee75274fc63fa12
Sergei <amonr51@gmail.com>
2019-03-04 00:45:24 +0300

    убрать рутер из 404, оставить только рутер Link

--- src/components/pages/default404.js
+++ src/components/pages/default404.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import './assets/css/default-404-page.css';
 
